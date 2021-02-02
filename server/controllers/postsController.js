1b6278fb17ede9052f456907f03ce4cd325bd445
Wendell Cardoso <cardosowendell@gmail.com>
2021-02-02 19:45:58 -0300

    listagem e criacao de posts

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -4,14 +4,22 @@ export const getPosts = async (req, res) => {
     try {
         const posts = await Post.find();
 
-        //console.log(posts);
-
         res.status(200).json(posts);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 };
 
-export const createPost = (req, res) => {
-    res.send('creating posts!');
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new Post(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+//import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,7 +11,7 @@ const app = express();
 
 app.use("/posts", postRoutes);
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }));
 //app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
