a94e9edaf86fda57347a31110c6705d845670a34
Wendell Cardoso <cardosowendell@gmail.com>
2021-02-03 19:52:55 -0300

    resolvendo bug payload

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -5,7 +5,7 @@ export const getPost = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
 
-        dispatch({ type: "FETCH_ALL", playload: data });
+        dispatch({ type: "FETCH_ALL", payload: data });
     } catch (error) {
         console.log(error.message);
     }
