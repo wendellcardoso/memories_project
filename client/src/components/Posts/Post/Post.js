f46b77692d6aefb581a3430756ba5b175157c6b8
Wendell Cardoso <cardosowendell@gmail.com>
2021-02-06 00:08:07 -0300

    finalizando componente Posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import Delete from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
 import useStyles from "./styles";
 
 const Post = () => {
