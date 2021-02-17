ffc68822c40c518436eac75977ae3f294f67849b
Wendell Cardoso <cardosowendell@gmail.com>
2021-02-17 16:10:28 -0300

    Layout

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import useStyles from "./styles";
+
+const Auth = () => {
+    // const classes = useStyles();
+    const state = null;
+
+    return (
+        <div>
+            AUTH
+        </div>
+    );
+};
+
+export default Auth;
